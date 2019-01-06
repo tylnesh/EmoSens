d277f34833f408c0568f55a730d0ec56f2c8ecce
tylnesh <kohutek.michal@gmail.com>
2019-01-06 22:12:43 +0100

    eyetrackick demo

---
+++ OpenCV_eyetracking/mainwindow.cpp
@@ -0,0 +1,80 @@
+#include <QDebug>
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+#include <opencv2/core/core.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
+#include <opencv2/objdetect/objdetect.hpp>
+#include <opencv2/imgproc.hpp>
+#include <opencv2/opencv.hpp>
+
+#define CV_BGR2GRAY cv::COLOR_BGRA2GRAY
+MainWindow::MainWindow(QWidget *parent) :
+    QMainWindow(parent),
+    ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+    cv::CascadeClassifier faceCascade;
+    cv::CascadeClassifier eyesCascade;
+
+
+    if (!faceCascade.load("/home/tylnesh/devel/diploma/OpenCV_eyetracking/haarcascade_frontalface_alt.xml"))
+      {
+          qDebug()  << "Could not load face detector." << endl;
+
+      }
+      if (!eyesCascade.load("/home/tylnesh/devel/diploma/OpenCV_eyetracking/haarcascade_eye_tree_eyeglasses.xml"))
+      {
+          qDebug() << "Could not load eye detector." << endl;
+
+      }
+
+
+
+    cv::VideoCapture cap(0); // the fist webcam connected to your PC
+      if (!cap.isOpened())
+      {
+          qDebug() << "webcam not available";
+      }
+      cv::Mat frame;
+      while (1)
+      {
+          cap >> frame; // outputs the webcam image to a Mat
+          detectEyes(frame, faceCascade, eyesCascade);
+          cv::imshow("Webcam", frame); // displays the Mat
+          if (cv::waitKey(30) >= 0) break; // takes 30 frames per second. if the user presses any button, it stops from showing the webcam
+      }
+
+
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
+
+
+void MainWindow::detectEyes(cv::Mat &frame, cv::CascadeClassifier &faceCascade, cv::CascadeClassifier &eyesCascade)
+{
+  cv::Mat grayscale;
+  cv::cvtColor(frame, grayscale, CV_BGR2GRAY); // convert image to grayscale
+  cv::equalizeHist(grayscale, grayscale); // enhance image contrast
+  std::vector<cv::Rect> faces;
+  faceCascade.detectMultiScale(grayscale, faces, 1.1, 2, 0 | cv::CASCADE_SCALE_IMAGE, cv::Size(150, 150));
+
+  if (faces.size() == 0) return; // none face was detected
+   cv::Mat face = frame(faces[0]); // crop the face
+   std::vector<cv::Rect> eyes;
+   eyesCascade.detectMultiScale(face, eyes, 1.1, 2, 0 |  cv::CASCADE_SCALE_IMAGE, cv::Size(150, 150)); // same thing as above
+
+   rectangle(frame, faces[0].tl(), faces[0].br(), cv::Scalar(255, 0, 0), 2);
+   if (eyes.size() != 2) return; // both eyes were not detected
+   for (cv::Rect &eye : eyes)
+   {
+       rectangle(frame, faces[0].tl() + eye.tl(), faces[0].tl() + eye.br(), cv::Scalar(0, 255, 0), 2);
+   }
+
+
+}
+
---
+++ OpenCV_eyetracking/mainwindow.h
@@ -0,0 +1,27 @@
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+#include <opencv2/core/core.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
+#include <opencv2/objdetect/objdetect.hpp>
+
+namespace Ui {
+class MainWindow;
+}
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    explicit MainWindow(QWidget *parent = nullptr);
+    ~MainWindow();
+
+private:
+    Ui::MainWindow *ui;
+    void detectEyes(cv::Mat &frame, cv::CascadeClassifier &faceCascade, cv::CascadeClassifier &eyeCascade);
+};
+
+#endif // MAINWINDOW_H
